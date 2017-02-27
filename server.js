452626bf15b7344214ea16a18aa3640474175454
rsmanjunath <rsmanjunath97@gmail.com>
2017-02-28 00:06:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.send(createTemplate(articleone));
+    res.send(createTemplate(Articleone));
    
 });
 
