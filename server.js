1b8a02a9d201a3b7459497dc37106d00bbf76182
rsmanjunath <rsmanjunath97@gmail.com>
2017-02-28 22:16:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,68 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
+var path = require('path'); 
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleone = {
- title: 'Article one|Manjunath',
- heading: 'Article one',
- date: 'feb 25,2017',
- content: `                                                                                    <p>
-                  This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-              </p>
-              <p>
-                  This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-              </p>
-             <p>
-                  This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-              </p> `
- 
-};
-
-function createTemplate (data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-}
-
-
-
-
-
-var htmlTemplate = `
-<html>
-   <head>
-       <title>
-           ${title}
-       </title>
-       <meta name="viewport" content="width=device-width, initial-scale=1" />
-       <link href="/ui/style.css" rel="stylesheet" />
-   </head>
-   <body>
-      <div class="container">
-          <div>
-          <a href="/">HOME</a>
-          </div>
-          </hr>
-          <h3>
-            ${heading}
-          </h3>
-          <div>
-              ${date}
-          </div>
-          <div>
-            ${content}
-              </div>
-        </div>  
-     </body>
-</html>
-`;
-return htmlTemplate`
-}
-
  
 
 app.get('/', function (req, res) {
@@ -70,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.send(createTemplate(articleone));
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     
 });
 
